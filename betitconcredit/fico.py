6bf6a0ffd7ebf0569a647586c358a83227eec10e
Justin Miller <justintime145@gmail.com>
2019-03-02 17:04:59 -0500

    Added comment to calc_credit_score function

--- betitconcredit/fico.py
+++ betitconcredit/fico.py
@@ -22,29 +22,52 @@ import math
 
 
 
-# monthsSinceMissedPayment is the number of months since the last missed payment
+# monthsSinceMissedPayment : number of months since the last missed payment
 def get_payment_history_points(monthsSinceMissedPayment: int) -> int:
     return 0
 
-# avgBalance is the average balance of all credit cards the player has
+# avgBalance : average balance of all credit cards the player has
 def get_outstanding_debt_points(avgBalance: int) -> int:
     return 0
 
-# numberMonths is the total number of months on the player's credit history,
+# numberMonths : total number of months on the player's credit history,
 # equivalent to the age in months of their oldest account.
 def get_credit_history_points(numberMonths: int) -> int:
     return 0
 
-# numberInquiriesLastSixMonths is the number of inquiries into the player's
+# numberInquiriesLastSixMonths : number of inquiries into the player's
 # credit in the last 6 months. Would be dictated by the scenario.
 def get_inquiries_points(numberInquiriesLastSixMonths: int) -> int:
     return 0
 
-# numberTradeLines is the number of credit accounts assosciated with the player. 
+# numberTradeLines : number of credit accounts assosciated with the player. 
 # This includes both credit cards and loans.
 def get_credit_mix_points(numberTradeLines: int) -> int:
     return 0
+"""
+Arg : meaning
 
+monthsSinceMissedPayment : number of months since the last missed payment
+        note : -1 indicates no missed payments
+
+avgBalance : average balance of all credit cards the player has
+        note : -1 indicates no credit cards (we won't see this in our usage, 
+        but just for reference.)
+
+numberMonths : total number of months on the player's credit history,
+  equivalent to the age in months of their oldest account.
+
+numberInquiriesLastSixMonths : number of inquiries into the player's
+  credit in the last 6 months. Would be dictated by the scenario.
+
+numberTradeLines : number of credit accounts assosciated with the player. 
+  This includes both credit cards and loans.
+"""
 def calc_credit_score(monthsSinceMissedPayment: int, avgBalance: int, numberMonths: int,
     numberInquiriesLastSixMonths: int, numberTradeLines: int) -> int:
-        
+    score = 505
+    score += get_credit_history_points(numberMonths)
+    score += get_credit_mix_points(numberTradeLines)
+    score += get_inquiries_points(numberInquiriesLastSixMonths)
+    score += get_outstanding_debt_points(avgBalance)
+    score += get_payment_history_points(monthsSinceMissedPayment)
