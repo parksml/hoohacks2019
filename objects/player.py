d645949f3c4867d2a3fe6e11b7cbf8b448c38c55
MicahParks <micahleviparks@gmail.com>
2019-03-02 14:15:23 -0500

    Boilerplate object.

---
+++ objects/player.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+[WHEN TO USE THIS FILE]
+[INSTRUCTIONS FOR USING THIS FILE]
+
+Project name: [MISSING]
+Author: Micah Parks
+
+This lives on the web at: [MISSING URL]
+Target environment: python 3.7
+"""
+
+# Start standard library imports.
+from random import randrange, uniform
+# End standard library imports.
+
+# Start third party imports.
+# End third party imports.
+
+# Start project imports.
+# End project imports.
+
+
+def get_random_account_balance_float() -> float:
+    """
+    """
+    return uniform(1000, 9000)
+
+
+def get_random_account_income_int() -> int:
+    """
+    """
+    return randrange(15000, 40000)
+
+
+def get_random_bill_percent_float() -> float:
+    """
+    """
+    return uniform(.3, .5)
+
+
+class Player:
+    def __init__(self, avatarUrlStr: str, nameStr: str) -> None:
+        self.accountBalanceFloat = get_random_account_balance_float()
+        self.avatarUrlStr = avatarUrlStr
+        self.creditScoreInt = 600
+        self.creditCardList = list()
+        self.incomeInt = get_random_account_income_int()
+        self.billTotalFloat = self.incomeInt * get_random_bill_percent_float()
+        self.jobBool = True
+        self.nameStr = nameStr
